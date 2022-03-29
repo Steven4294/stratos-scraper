ae1dbfb316e7944f70ac88a1d8d9be64840b71b5
Steven Petteruti <stevenpetterutijr@gmail.com>
2022-03-29 01:57:11 -0400

    push

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,8 @@ const puppeteer = require('puppeteer');
 const proxyChain = require('proxy-chain');
 import * as schedule from "node-schedule";
 
-(async() => {
-	
- 
-})();
 
-async function main() {
-
- 
-}
-
-
-const url = 'https://www.google.com/'
- // const base = 'http://127.0.0.1:8080'
- 
-
+async function main() { }
 
 async function init() {
   const oldProxyUrl = 'http://3t3mf:x4nbdt2h@169.197.83.75:25383';
@@ -29,12 +16,14 @@ async function init() {
   });
 
   const page = await browser.newPage();
-  await page.goto('https://www.pokersiteinfo.com/landing');
-  // await page.goto('http://localhost:4200/landing');
+  // await page.goto('https://www.pokersiteinfo.com/landing');
+  await page.goto('http://localhost:4200/landing');
   // const element = await page.$('pre');
-  await page.type('#form', 'Test Name')
-  // const text = await page.evaluate((element: { textContent: any; }) => element.textContent, element);
+  const element = await page.$('#title')
+   await page.type('#form', 'Test Name')
+  const text = await page.evaluate((element: { textContent: any; }) => element.textContent, element);
   // // await page.screenshot({ path: 'screenshots/1.png'})
+  console.log(text)
   // console.log('origin')
   // console.log(text);
   await page.waitFor(500);
@@ -52,7 +41,7 @@ await page.waitFor(500);
 }
 
 main().then(async () => {
-	  
+	  init()
     schedule.scheduleJob(`10 * * * * *`, async () => {
 		  //every 100 seconds
       await init()
