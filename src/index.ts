f26fdfcffdbcc589d12c18f45e8ff312c590748d
Steven Petteruti <stevenpetterutijr@gmail.com>
2022-03-29 01:24:45 -0400

    push

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 require('@babel/polyfill');
 const puppeteer = require('puppeteer');
 const proxyChain = require('proxy-chain');
+import * as schedule from "node-schedule";
 
 (async() => {
 	
-  const oldProxyUrl = 'http://3t3mf:x4nbdt2h@169.197.83.75:25383';
-  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-
-  const browser = await puppeteer.launch({
-    args: [`--proxy-server=${newProxyUrl}`, '--no-sandbox'],
-  });
-
-  const page = await browser.newPage();
-  await page.goto('https://httpbin.org/ip');
-  const element = await page.$('pre');
-  const text = await page.evaluate((element: { textContent: any; }) => element.textContent, element);
-  await page.screenshot({ path: 'screenshots/1.png'})
-  console.log('origin')
-  console.log(text);
-  await browser.close();
-
-  await proxyChain.closeAnonymizedProxy(newProxyUrl, true);
+ 
 })();
 
 async function main() {
@@ -35,19 +20,44 @@ const url = 'https://www.google.com/'
 
 
 async function init() {
-  	try {
-		// await driver.get(url);
-		// const aboutBtn = await driver.findElement(By.xpath('/html/body/div[1]/div[1]/a[1]'))
-		// const text = await aboutBtn.getText()
-		// console.log(text)
- 
-    } finally {
-		
-	}
+  const oldProxyUrl = 'http://3t3mf:x4nbdt2h@169.197.83.75:25383';
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [`--proxy-server=${newProxyUrl}`, '--no-sandbox'],
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.pokersiteinfo.com/landing');
+  // await page.goto('http://localhost:4200/landing');
+  // const element = await page.$('pre');
+  await page.type('#form', 'Test Name')
+  // const text = await page.evaluate((element: { textContent: any; }) => element.textContent, element);
+  // // await page.screenshot({ path: 'screenshots/1.png'})
+  // console.log('origin')
+  // console.log(text);
+  await page.waitFor(500);
+
+  await page.focus('#submit')
+  await page.focus('#submit')
+
+await page.keyboard.type('\n');
+
+await page.waitFor(500);
+
+
+  await browser.close();
+  await proxyChain.closeAnonymizedProxy(newProxyUrl, true);
 }
 
 main().then(async () => {
-	await init()
+	  
+    schedule.scheduleJob(`10 * * * * *`, async () => {
+		  //every 100 seconds
+      await init()
+      console.log('console.log')
 
+	  });
 
 })
